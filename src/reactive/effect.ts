6c448c9a8654ebf3559db6815fe5f68405d2c254
derlan <1372545550@qq.com>
2022-05-01 18:12:51 +0800

    feat:stop

--- src/reactive/effect.ts
+++ src/reactive/effect.ts
@@ -1,4 +1,6 @@
 export class ReactiveEffect {
+  public deps: Set< Set<ReactiveEffect>> = new Set()
+  public active = true
   constructor(private _fn: Function, public scheduler?: Function) {
     this._fn = _fn
     this.scheduler = scheduler
@@ -7,6 +9,17 @@ export class ReactiveEffect {
   public run() {
     return this._fn()
   }
+
+  public stop() {
+    if (!this.active)
+      return
+    this.active = false
+    clearEffects(this)
+  }
+}
+function clearEffects(reactiveEffect: ReactiveEffect) {
+  for (const dep of reactiveEffect.deps)
+    dep.delete(reactiveEffect)
 }
 let activeEffect: ReactiveEffect | undefined
 export type EffectMap=Map<string | symbol, Set<ReactiveEffect>>
@@ -20,8 +33,11 @@ export function track(target: object, key: string | symbol) {
   let effects = effectMap.get(key)
   if (!effects)
     effectMap.set(key, effects = new Set())
-  if (activeEffect && !effects.has(activeEffect))
+  if (activeEffect && !effects.has(activeEffect)) {
     effects.add(activeEffect)
+    // 把储存了这个依赖的set存入ReactiveEffect的deps中 这样ReactiveEffect就知道被哪些依赖了
+    activeEffect.deps.add(effects)
+  }
   return effects
 }
 
@@ -39,11 +55,21 @@ export function trigger(target: object, key: string | symbol) {
       effect.run()
   }
 }
-export function effect(fn: Function, options?: { scheduler?: Function }) {
+export interface ReactiveEffectRunner<T = any> {
+  (): T
+  effect: ReactiveEffect
+}
+export function effect(fn: Function, options?: { scheduler?: Function }): ReactiveEffectRunner {
   const { scheduler } = options || {}
   const _effect = new ReactiveEffect(fn, scheduler)
   activeEffect = _effect
   _effect.run()
   activeEffect = undefined
-  return _effect.run.bind(_effect)
+  const runner: ReactiveEffectRunner = _effect.run.bind(_effect) as ReactiveEffectRunner
+  runner.effect = _effect
+  return runner
+}
+
+export function stop(runner: ReactiveEffectRunner) {
+  runner.effect.stop()
 }
