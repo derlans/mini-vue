7246dfcb10a658112893ced750fa8d872982e765
derlan <1372545550@qq.com>
2022-05-02 21:37:10 +0800

    feat:hasChanged

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,3 +3,6 @@ export const extend = Object.assign
 export function isObject(value: unknown): value is object {
   return value !== null && typeof value === 'object'
 }
+export function hasChanged(value: any, oldValue: any): boolean {
+  return !Object.is(value, oldValue)
+}
