bfe58a811b362177e1ed6b49f2a956eeeaaae945
derlan <1372545550@qq.com>
2022-05-02 11:22:45 +0800

    feat:isReactive isReadonly

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -1,7 +1,19 @@
 import { reactiveHandler, readonlyHandler } from './baseHandlers'
+export enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive__',
+  IS_READONLY = '__v_isReadonly__',
+}
 export function reactive<T extends object>(raw: T): T {
   return new Proxy(raw, reactiveHandler)
 }
 export function readonly<T extends object>(raw: T): T {
   return new Proxy(raw, readonlyHandler)
+}
+
+export function isReactive(value: unknown): boolean {
+  return !!(value && (value as any)[ReactiveFlags.IS_REACTIVE])
+}
+
+export function isReadonly(value: unknown): boolean {
+  return !!(value && (value as any)[ReactiveFlags.IS_READONLY])
 }
