2789a35ed4229427c159b5be87ef630c7b0a2e27
derlan <1372545550@qq.com>
2022-05-22 18:40:01 +0800

    refactor

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -3,6 +3,6 @@ import type { ComponentInstance } from './component'
 export function emit(component: ComponentInstance, event: string, ...args: any[]) {
   const props = component.props as any
   const eventName = `on${capitalize(event)}`
-  const emit = props[eventName]
-  emit && emit(...args)
+  const handler = props[eventName]
+  handler && handler(...args)
 }
